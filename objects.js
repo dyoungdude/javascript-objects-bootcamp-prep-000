b0b6841ac4b464f134486023947ef71afc4c7ada
Darren Young <dlyoung5@asu.edu>
2018-07-17 15:30:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,4 @@
-var playlist = new Object();
-
-function updatePlaylist(playlist, artistName, songTitle){
-    playlist['Slowdive'] = 'Alison';
-    playlist['My Bloody Valentine'] = 'Sometimes';
-    return playlist
-}
+var playlist = {Slowdive: 'Alison', My Bloody Valentine: 'Sometimes'};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs'] = "Here's to the State of Mississippi";
