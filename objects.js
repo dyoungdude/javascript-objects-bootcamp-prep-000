87ece3d9ecba5c568694d0c8634d3496f43ae39d
Darren Young <dlyoung5@asu.edu>
2018-07-17 15:08:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,12 @@ var playlist = {Slowdive: 'Alison', My Bloody Valentine: 'Sometimes'};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs'] = "Here's to the State of Mississipi";
+  console.log(playlist);
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist.Slowdive;
+  console.log(playlist);
   return playlist;
 }
